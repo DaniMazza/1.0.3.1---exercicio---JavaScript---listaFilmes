5679bfe3c73a8995791d61307c1f39f3d9526ade
Dani G. Mazzarolo <145726469+DaniMazza@users.noreply.github.com>
2024-03-08 22:23:00 -0300

    Add files via upload

---
+++ 1.0.3.1.js
@@ -0,0 +1,43 @@
+const filmes = [
+    {
+    favorito: true,
+    nomeOriginal: 'WHEN MARNIE WAS THERE',
+    nomeBrasileiro: 'Memórias de Marnie',
+    produtora: 'Studio Ghibli',
+    dataDeLancamento: 2014,
+    generos: [
+        'animação',
+        'fantasia',
+        'aventura',
+        'drama',
+    ],
+    
+    criticas: [73, 73, 73],
+    },
+    {
+    favorito : false,
+    nomeOriginal: 'The Maze Runner',
+    nomeBrasileiro: 'Maze Runner: Correr ou Morrer',
+    produtora: 'Temple Hill Entertainment',
+    dataDeLancamento: 2014,
+    generos: [
+        'ação',
+        'ficção científica',
+        'mistério'
+    ],
+    criticas: [63, 63, 58],
+   },
+    {
+    favorito: true, 
+    nomeOriginal: 'Hauru no Ugoku Shiro',
+    nomeBrasileiro: 'O Castelo Animado',
+    produtora: 'Studio Ghibli',
+    dataDeLancamento: 2004,
+    generos: [
+        'aventura',
+        'fantasia'
+    ],
+    criticas: [87, 93, 87],
+    },
+];
+console.log(filmes[2])
